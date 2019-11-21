b7c153e8ae6791dea04237851983576e7db8f31e
ShinoharaHare <anna.shike1822@gmail.com>
2019-11-22 04:08:16 +0800

    試著將主頁面及註冊頁面整合至 Vue CLI 了
    @shinoharahare
    HTML:
        靜態檔案放到 asserts 後，以 @asserts/<file_name> 來引用
    
    CSS:
        scoped 會導致動態載入的元素無法套用CSS
        img 必須要有 width 和 heigh 否則會變形
    
    JS:
        記得將事件綁定等指令放到 mounted 裡
    
    @wayne1224
    1. 記得把一個元件用 div 包起來(id以那個元件的用途命名)，並將CSS裡的 body 設定改成該 div
    2. 之後要把頁面用到的多個 JS、CSS 個別整合成一個 .js 檔及 .css 檔
    3. CSS 設定 img 時記得把 width 跟 heigh 都補上
    
    Co-Authored-By: wayne1224 <wayne1224@users.noreply.github.com>

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
+import Main from '@/components/main/index';
+import Member from '@/components/member/index';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Main',
+      component: Main
+    }, 
+    {
+      path: '/member',
+      name: 'Member',
+      component: Member
     }
   ]
-})
+});
