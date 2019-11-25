bec36b5ca240884098d8be4c15ba87629d87a2ad
ShinoharaHare <anna.shike1822@gmail.com>
2019-11-25 23:03:59 +0800

    登入畫面背景

--- src/components/member/script.js
+++ src/components/member/script.js
@@ -1,61 +1,45 @@
-import $ from 'jquery';
 import sha256 from 'sha256';
-import 'prefixfree';
-
-function login(username, password) {
-    return fetch(`${location.origin}/api/member?action=login`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username: username, hash: sha256(password) })
-    });
-}
-
-function register(username, password, email) {
-    return fetch(`${location.origin}/api/member?action=register`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username: username, hash: sha256(password), email: email })
-    });
-}
 
 export default {
     name: 'Member',
-    mounted() {
-        $('.info-item .btn').click(() => {
-            $('.container').toggleClass('log-in');
-        });
-
-        $('.form-item.log-in .btn').click(() => {
-            var username = $('.log-in input[name="username"]').val();
-            var password = $('.log-in input[name="password"]').val();
-
-            if (true) {
-                login(username, password).then((res) => {
-                    return res.json();
-                }).then((json) => {
-                    if (json.code === 0) {
-                        $('.container').addClass('active');
-                        setTimeout(() => { location.href = '/'; }, 1000);
-                    }
-                });
-            }
-        });
-
-        $('.form-item.sign-up .btn').click(() => {
-            var username = $('.sign-up input[name="username"]').val();
-            var password = $('.sign-up input[name="password"]').val();
-            var email = $('.sign-up input[name="email"]').val();
-
-            if (true) {
-                register(username, password, email).then((res) => {
-                    return res.json();
-                }).then((json) => {
-                    if (json.code === 0) {
-                        $('.container').addClass('active');
-                        setTimeout(() => { location.href = '/'; }, 1000);
-                    }
-                });
-            }
-        });
+    data: () => Object({
+        isRegisterForm: false,
+        username: '',
+        password: '',
+        repeatPassword: '',
+        email: '',
+        success: false
+    }),
+    methods: {
+        switchToLogin() {
+            this.isRegisterForm = false;
+        },
+        switchToRegister() {
+            this.isRegisterForm = true;
+        },
+        login() {
+            fetch('api/member/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ username: this.username, hash: sha256(this.password) })
+            })
+            .then((res) => res.json())
+            .then((json) => { 
+                this.success = json.success;
+                setTimeout(() => location.href = '/', 1000);
+            });
+        },
+        register() {
+            fetch('api/member/register', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ username: this.username, hash: sha256(this.password), email: this.email })
+            })
+            .then((res) => res.json())
+            .then((json) => { 
+                this.success = json.success;
+                setTimeout(() => location.href = '/', 1000);
+            });
+        }
     }
 }
