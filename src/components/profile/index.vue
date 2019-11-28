3ae66de29446771f09bdf1d5c6f8a9062269e189
ShinoharaHare <anna.shike1822@gmail.com>
2019-11-28 22:45:45 +0800

    加上暫定 profile

--- src/components/profile/index.vue
+++ src/components/profile/index.vue
@@ -1,13 +1,34 @@
 <template>
-  <modal name="profile" :draggable="true">
+  <modal name="profile" :width="800" :height="602">
     <div id="profile-component">
-      <div class="wrap">
-        <div class="shape sec"></div>
-        <div class="shape min"></div>
-        <div class="shape hr"></div>
-        <div class="cir"></div>
+      <div class="portfoliocard">
+        <div class="coverphoto"></div>
+        <div class="profile_picture"></div>
+        <div class="left_col">
+          <div class="followers">
+            <div class="follow_count">100</div>Freinds
+          </div>
+          <!-- <div class="following">
+					<div class="follow_count">181</div>
+					Following
+          </div>-->
+        </div>
+        <div class="right_col">
+          <h2 class="name">User name</h2>
+          <h3 class="location">Taiwan Taipei</h3>
+          <ul class="contact_information">
+            <li class="work">student</li>
+            <li class="website">
+              <a class="nostyle" href="#">www.facebook.com</a>
+            </li>
+            <li class="mail">gginin@test.com</li>
+            <li class="phone">0800-092-000</li>
+            <li class="resume">
+              <a href="#" class="nostyle">download resume</a>
+            </li>
+          </ul>
+        </div>
       </div>
-      <p>Every second is of infinite value</p>
     </div>
   </modal>
 </template>
