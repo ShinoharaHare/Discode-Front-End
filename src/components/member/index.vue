55587b8f32655754179118207da3292b37deeec4
ShinoharaHare <anna.shike1822@gmail.com>
2019-12-04 03:03:40 +0800

    member 頁面完成

--- src/components/member/index.vue
+++ src/components/member/index.vue
@@ -6,7 +6,7 @@
     <div id="title" class="unselectable">
       <span>Discode</span>
     </div>
-    <div class="container unselectable" :class="{'log-in': isRegisterForm, 'active': success}">
+    <div class="container unselectable" :class="{'log-in': form, 'active': success}">
       <div class="box"></div>
       <div class="container-forms">
         <div class="container-info">
@@ -14,7 +14,7 @@
             <div class="table">
               <div class="table-cell">
                 <p>有帳號了嗎?</p>
-                <div class="btn" @click="isRegisterForm = false;">登入</div>
+                <div class="btn" @click="switchForm(false);">登入</div>
               </div>
             </div>
           </div>
@@ -22,7 +22,7 @@
             <div class="table">
               <div class="table-cell">
                 <p>還沒有帳號嗎?</p>
-                <div class="btn" @click="isRegisterForm = true;">註冊</div>
+                <div class="btn" @click="switchForm(true);">註冊</div>
               </div>
             </div>
           </div>
@@ -32,13 +32,13 @@
           <div class="form-item log-in">
             <div class="table">
               <div class="table-cell">
-                <div :class="{'error': errors[0]}">
-                  <p>用戶名{{errorLabels[0]}}</p>
+                <div :class="{'error': errorLabels[0]}">
+                  <p>用戶名&nbsp;{{errorLabels[0]}}</p>
                   <input name="username" placeholder="用戶名" type="text" v-model="username" />
                 </div>
 
-                <div :class="{'error': errors[1]}">
-                  <p>密碼{{errorLabels[1]}}</p>
+                <div :class="{'error': errorLabels[1]}">
+                  <p>密碼&nbsp;{{errorLabels[1]}}</p>
                   <input name="password" placeholder="密碼" type="password" v-model="password" />
                 </div>
 
@@ -50,17 +50,17 @@
           <div class="form-item sign-up">
             <div class="table">
               <div class="table-cell">
-                <div :class="{'error': errors[0]}">
-                  <p>用戶名{{errorLabels[0]}}</p>
+                <div :class="{'error': errorLabels[0]}">
+                  <p>用戶名&nbsp;{{errorLabels[0]}}</p>
                   <input placeholder="用戶名" type="text" v-model="username" />
                 </div>
-                <div :class="{'error': errors[1]}">
-                  <p>密碼{{errorLabels[1]}}</p>
+                <div :class="{'error': errorLabels[1]}">
+                  <p>密碼&nbsp;{{errorLabels[1]}}</p>
                   <input placeholder="密碼" type="password" v-model="password" />
                 </div>
 
-                <div :class="{'error': errors[2]}">
-                  <p>確認密碼{{errorLabels[2]}}</p>
+                <div :class="{'error': errorLabels[2]}">
+                  <p>確認密碼&nbsp;{{errorLabels[2]}}</p>
                   <input placeholder="確認密碼" type="password" v-model="confirmPassword" />
                 </div>
 
--- src/components/member/script.js
+++ src/components/member/script.js
@@ -1,23 +1,32 @@
 import sha256 from 'sha256';
-
-// [A-Za-z0-9_]+
-// ^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$
-
+import error from '@/error'
 
 export default {
     name: 'Member',
     data: () => Object({
-        isRegisterForm: false,
+        form: false,
+        success: false,
         username: '',
         password: '',
         confirmPassword: '',
-        success: false,
         errorLabels: ['', '', ''],
-        errors: [false, false, false],
     }),
     methods: {
+        switchForm(bool) {
+            this.errorLabels = ['', '', ''];
+            this.form = bool;
+        },
         login() {
-            if (this.username && this.password) {
+            this.errorLabels = ['', '', ''];
+            if (this.username === '') {
+                this.errorLabels[0] = '為必填欄位';
+            } 
+
+            if (this.password === '') {
+                this.errorLabels[1] = '為必填欄位';
+            }
+
+            if (this.errorLabels.every(x => x === '')) {
                 fetch('api/member/login', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
@@ -25,49 +34,64 @@ export default {
                 })
                     .then((res) => res.json())
                     .then((json) => {
-                        this.success = json.success;
-                        setTimeout(() => location.href = '/', 1000);
-                    });
-            } else {
+                        if (json.success) {
+                            this.success = true;
+                            setTimeout(() => location.href = '/', 1000);
+                        } else {
+                            switch (json.code) {
+                                case error.UserNotFoundError.code:
+                                    this.errorLabels[0] = json.msg;
+                                    break;
 
+                                case error.PasswordInvalidError.code:
+                                    this.errorLabels[1] = json.msg;
+                                    break;
+                            }
+                            // Vue 的問題
+                            this.errorLabels = JSON.parse(JSON.stringify(this.errorLabels));
+                        }
+                    });
             }
         },
         register() {
-            if (!/[A-Za-z0-9_]+/.test(this.username)) {
-                
-                return;
+            this.errorLabels = ['', '', ''];
+            
+            if (this.username === '') {
+                this.errorLabels[0] = '為必填欄位';
+            } else if (!/[A-Za-z0-9_]+/.test(this.username)) {
+                this.errorLabels[0] = '必須是大小寫字母、數字或底線';
             }
 
-            if (!/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(this.password)) {
-
-                return;
+            if (this.password === '') {
+                this.errorLabels[1] = '為必填欄位';
+            } else if (!/[A-Za-z\d]{8,}/.test(this.password)) {
+                this.errorLabels[1] = '必須是大小寫字母、數字且至少8個字元';
+            } else if (this.password != this.confirmPassword) {
+                this.errorLabels[2] = '兩次輸入不一致';
             }
             
-            if (this.password !== this.confirmPassword) {
-
-                return;
+            if (this.errorLabels.every(x => x === '')) {
+                fetch('api/member/register', {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({ username: this.username, hash: sha256(this.password) })
+                })
+                    .then((res) => res.json())
+                    .then((json) => {
+                        if (json.success) {
+                            this.success = true;
+                            setTimeout(() => location.href = '/', 1000);
+                        } else {
+                            switch (json.code) {
+                                case error.UsernameDuplicateError.code:
+                                    this.errorLabels[0] = json.msg;
+                                    break;
+                            }
+                            // Vue 的問題
+                            this.errorLabels = JSON.parse(JSON.stringify(this.errorLabels));
+                        }
+                    });
             }
-
-            fetch('api/member/register', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ username: this.username, hash: sha256(this.password) })
-            })
-                .then((res) => res.json())
-                .then((json) => {
-                    this.success = json.success;
-                    setTimeout(() => location.href = '/', 1000);
-                });
         },
-        usernameField() {
-            this.errors[0] = /[A-Za-z0-9_]+/.test(this.username);
-            console.log(this.username ? !this.validFlags[0] : false)
-        },
-        passwordField() {
-            this.errors[1] = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/.test(this.password);
-        },
-        confirmPasswordField() {
-            this.errors[2] = this.confirmPassword === this.password;
-        }
     }
 }
