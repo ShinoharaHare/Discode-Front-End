23e7dafa5df9fb6ef8fab800d616e69dff4b1946
ShinoharaHare <anna.shike1822@gmail.com>
2019-12-04 23:34:18 +0800

    Update error.js

--- src/error.js
+++ src/error.js
@@ -1,1 +1,24 @@
-D:/Projects/專題/Discode-Back-End/common/error.js
+const error = {
+    // 未知
+    UnknownError: '未知的錯誤',
+    // token
+    TokenNotValidError: '無效的Token',
+    TokenNotProvidedError: '未提供Token',
+    TokenExpiredError: '過期的Token',
+    // 登入
+    UserNotFoundError: '找不到此用戶',
+    PasswordInvalidError: '密碼錯誤',
+    // 註冊
+    UsernameDuplicateError: '用戶名已存在'
+};
+
+var _code = 0;
+for (let name in error) {
+    error[name] = {
+        code: _code++,
+        name: name,
+        msg: error[name]
+    }
+}
+
+module.exports = error;
